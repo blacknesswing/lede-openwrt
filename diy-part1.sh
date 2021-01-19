bc34d5dfc966c44a6a520da7f4059e955f2a6c33
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-01-19 22:10:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -52,8 +52,8 @@ git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos  #�
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
 
 # wifi
-svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/rtl8812au-ac package/lean/rtl8812au-ac
-svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/rtl8821cu package/lean/rtl8821cu
-svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/rtl88x2bu package/lean/rtl88x2bu
-svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/mt7601u-ap package/lean/mt7601u-ap
+svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/rtl8812au-ac package/ctcgfw/rtl8812au-ac
+svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/rtl8821cu package/ctcgfwrtl8821cu
+svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/rtl88x2bu package/ctcgfw/rtl88x2bu
+svn co https://github.com/1715173329/openwrt/tree/openwrt-18.06/package/ctcgfw/mt7601u-ap package/ctcgfw/mt7601u-ap
 
