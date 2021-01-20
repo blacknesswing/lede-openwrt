10cb5a6e8781d15f55230ee2e5553569981f84ec
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-01-20 16:42:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,9 +23,13 @@
 # cd openwrt
 #rm -rf package/lean/luci-theme-argon 
 
-#京东签到
+# 京东签到
 # git clone https://github.com/jerrykuku/node-request package/node-request
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+
+# Vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 #oled
 git clone https://github.com/NateLol/luci-app-oled package/luci-app-oled
@@ -57,7 +61,7 @@ git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos  #�
 
 
 # argon-主题
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  
 #全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
