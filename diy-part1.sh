8c2bc4a31dbc852dfe2e4ffae368949cfe2f2d1a
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-01-20 22:14:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,6 +23,13 @@
 cd openwrt
 rm -rf package/lean/luci-theme-argon 
 
+# argon-主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+#全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
+#增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
+#增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
+
 # 京东签到
 # git clone https://github.com/jerrykuku/node-request package/node-request
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
@@ -38,7 +45,7 @@ git clone https://github.com/NateLol/luci-app-oled package/luci-app-oled
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 
 #主题-edge-动态登陆界面
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  
+# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  
 
 # openclash出国软件
 git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
@@ -58,14 +65,6 @@ git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos  #�
 
 # adguardhome
 # svn co https://github.com/project-openwrt/openwrt/branches/master/package/ctcgfw/luci-app-adguardhome package/ctcgfw/luci-app-adguardhome
-
-
-# argon-主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-#全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
-#增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
-#增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
 
 # wifi驱动R2s
 svn co https://github.com/project-openwrt/openwrt/branches/master/package/ctcgfw/rtl8821cu package/ctcgfw/rtl8821cu
