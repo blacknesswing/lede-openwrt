a245ca97b4013bc0378f5e1c91ee86a850e1af20
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-01-20 21:59:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -62,7 +62,7 @@ git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos  #�
 
 # argon-主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 #全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
