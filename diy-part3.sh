b34971a379bd5957c8f7107537f68a791f884dac
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-03-04 21:07:28 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -28,8 +28,3 @@ sed -i "s/OpenWrt /Build by Blacknesswing | OpenWrt Lean /g" package/lean/defaul
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-wget https://github.com/quintus-lab/Openwrt-R2S/raw/master/patches/999-unlock-1608mhz-rk3328.patch
-cp 999-unlock-1608mhz-rk3328.patch target/linux/rockchip/patches-5.4/
-sed -i 's/003-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch/999-unlock-1608mhz-rk3328.patch/g' /target/linux/rockchip/patches-5.4/
-
-
