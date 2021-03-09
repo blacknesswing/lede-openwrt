5b799ae33eb82eae4ffa185ad34e1e4df62d40ed
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2021-03-09 23:52:25 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -18,6 +18,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # 修改机器名称
 sed -i 's/OpenWrt/OpenWrt-R2s/g' package/base-files/files/bin/config_generate
+# SWAP LAN WAN 满足千兆场景，可选
+sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 #修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大源码那里target/linux查看，对应修改下面的路径就好）
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
