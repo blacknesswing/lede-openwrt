0f5dd189b4af781bac0a002d7fbb858b81af7ece
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2023-11-04 20:43:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -46,8 +46,8 @@ svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/kernel
 # git clone https://github.com/281677160/openwrt-package package/luci-app-cpulimit
 
 # Vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 #oled
 git clone https://github.com/NateLol/luci-app-oled package/luci-app-oled
