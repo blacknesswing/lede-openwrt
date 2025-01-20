90ac74b1deacd3bf31a89b5c67c3ff13ccb071c9
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2025-01-20 11:20:08 +0800

    Update diy-23.sh

--- diy-23.sh
+++ diy-23.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/luci2/bin/config_genera
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改机器名称
-sed -i 's/OpenWrt/OpenWrt-X86/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/OpenWrt-X86/g' package/base-files/files/bin/config_generate
 
 #修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大源码那里target/linux查看，对应修改下面的路径就好）
 # sed -i "s/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.4/g" target/linux/x86/Makefile
