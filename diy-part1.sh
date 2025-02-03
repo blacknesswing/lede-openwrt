4569b397bd0f038c59571a267f7a1c6a906b20ed
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2025-02-04 02:11:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -43,6 +43,13 @@
 rm -rf package/lean/luci-app-passwall2 && git clone -f https://github.com/xiaorouji/openwrt-passwall2 package/lean/luci-app-passwall2
 rm -rf package/lean/luci-app-passwall && git clone https://github.com/xiaorouji/openwrt-passwall package/lean/luci-app-passwall
 rm -rf package/lean/luci-app-openclash && git clone https://github.com/xiaorouji/openwrt-openclash package/lean/luci-app-openclash
+
+#cd package/lean/luci-app-passwall2 && git fetch --all && git reset --hard origin/main && git clean -fd
+#cd package/lean/luci-app-passwall && git fetch --all && git reset --hard origin/main && git clean -fd
+#cd package/lean/luci-app-openclash && git fetch --all && git reset --hard origin/main && git clean -fd
+
+
+
 # Vssr
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
