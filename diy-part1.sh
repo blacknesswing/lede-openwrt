b609a231c1a275252bba7ae35ad44bbacd98ec87
blacknesswing <42890894+blacknesswing@users.noreply.github.com>
2025-02-04 14:06:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #添加订阅源
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >>feeds.conf.default
+# echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >>feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 # echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
